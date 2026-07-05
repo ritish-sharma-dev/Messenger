46db8b17243f6a080922bc17966ae90fd24b4264
Ritish Sharma <ritishsharma04022006@gmail.com>
2026-07-06 00:27:41 +0530

    fix: port binding for production

--- server/src/index.js
+++ server/src/index.js
@@ -22,11 +22,9 @@ app.use("/api/messages", messageRoutes);
 // CONNECT TO MONGODB DATABASE
 await connectDB();
 
-if (process.env.NODE_ENV !== "production") {
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
-    });
-}
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
 
 export default server;
